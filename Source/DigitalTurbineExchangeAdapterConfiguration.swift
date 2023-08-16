dda6ab5af0846e3a28466c28b21e663de4e1c65b
Ray Graham <rayg@bitwisedesign.com>
2023-08-16 11:44:55 -0700

    Apply suggestions from code review

--- Source/DigitalTurbineExchangeAdapterConfiguration.swift
+++ Source/DigitalTurbineExchangeAdapterConfiguration.swift
@@ -10,7 +10,7 @@ import os.log
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
 @objc public class DigitalTurbineExchangeAdapterConfiguration: NSObject {
 
-    private static var log = OSLog(subsystem: "com.chartboost.mediation.adapter.fyber", category: "Configuration")
+    private static let log = OSLog(subsystem: "com.chartboost.mediation.adapter.fyber", category: "Configuration")
 
     /// Flag that can optionally be set to change the log level of the Digital Turbine Exchange SDK.
     @objc public static var logLevel: DTXLogLevel = .info {
