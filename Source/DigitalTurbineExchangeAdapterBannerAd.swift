6e54a384cdeaeb9750b710a1cc25a32daae1c25e
Daniel Barros <daniel.barros@chartboost.com>
2024-04-18 10:02:36 +0530

    Update show method

--- Source/DigitalTurbineExchangeAdapterBannerAd.swift
+++ Source/DigitalTurbineExchangeAdapterBannerAd.swift
@@ -73,15 +73,7 @@ final class DigitalTurbineExchangeAdapterBannerAd: DigitalTurbineExchangeAdapter
             }
         }
     }
-    
-    /// Shows a loaded ad.
-    /// It will never get called for banner ads. You may leave the implementation blank for that ad format.
-    /// - parameter viewController: The view controller on which the ad will be presented on.
-    /// - parameter completion: Closure to be performed once the ad has been shown.
-    func show(with viewController: UIViewController, completion: @escaping (Result<PartnerDetails, Error>) -> Void) {
-        /// NO-OP
-    }
-    
+
     /// Build a partner ad request.
     /// - Parameter placement: The placement ID for the ad request.
     /// - Returns: A partner ad request for the current Chartboost Mediation ad load.
